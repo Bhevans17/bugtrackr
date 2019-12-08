9fb2170d78a56b32c0a25e989a77b655998a2df9
Brenden <bhevans17@gmail.com>
2019-12-07 21:48:02 -0800

    Added styles to login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php include './includes/header.php'; ?>
 
 
-<div class="container">
+<div class="container my-5">
     <div class="row">
         <div class="col-md-4 offset-md-4">
             <h1>Login</h1>
