aaab7d0acaa257b2578f428ae08d11170fbf8595
Brenden <bhevans17@gmail.com>
2020-01-27 11:26:23 -0800

    Added dashboard styles

--- dashboard.php
+++ dashboard.php
@@ -13,50 +13,54 @@
 <body>
     <!-- Dashboard-->
     <!-- Navigation Bar -->
-    <div class="container-fluid">
+    <div class="container-fluid border-bottom">
         <div class="row">
             <div class="col-md-12">
-                <nav class="navbar navbar-expand-lg navbar-light">
-                    <span class="navbar-brand"><i class="fas fa-spider"></i>&nbsp;Dashboard</span>
-                    <button class="navbar-toggler" data-target="#navbarSupportedContent" aria-lable="Toggle navigation" type="button" aria-expanded="false" data-toggle="collapse">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+                <div class="container">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <span class="navbar-brand"><i class="fas fa-spider"></i>&nbsp;Dashboard</span>
+                        <button class="navbar-toggler" data-target="#navbarSupportedContent" aria-lable="Toggle navigation" type="button" aria-expanded="false" data-toggle="collapse">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ml-auto">
-                            <li class="nav-item dropdown">
-                                <a href="" class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"">
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item dropdown">
+                                    <a href="" class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"">
                                     <i class=" fas fa-user"></i>&nbsp;Username
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="#">Action</a>
-                                    <a class="dropdown-item" href="#">Another action</a>
-                                    <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="#">Something else here</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="#">Action</a>
+                                        <a class="dropdown-item" href="#">Another action</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="#">Something else here</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
             </div>
         </div>
     </div>
     <!-- Main Section-->
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-4">
-                <section id="dashboard-side-menu">
-                    <nav class="nav flex-column">
-                        <a class="nav-link active" href="#">Active</a>
-                        <a class="nav-link" href="#">Link</a>
-                        <a class="nav-link" href="#">Link</a>
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </nav>
-                </section>
+    <section id="main-section">
+        <div class="container-fluid h-100">
+            <div class="row justify-content-center h-100">
+                <div class="col-md-4 bg-dark">
+                    <section id="dashboard-side-menu">
+                        <nav class="nav flex-column">
+                            <a class="nav-link active" href="#">Active</a>
+                            <a class="nav-link" href="#">Link</a>
+                            <a class="nav-link" href="#">Link</a>
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </nav>
+                    </section>
+                </div>
+                <div class="col-md-8 bg-light">asdf</div>
             </div>
-            <div class="col-md-8">asdf</div>
         </div>
-    </div>
-
+    </section>
     <script src="https://kit.fontawesome.com/b836e57116.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
